f5d21ecf3bd9019e2c3cf307e97c1dada7c86f0f
Darth2499 <imsk8erboi@gmail.com>
2021-04-06 20:12:51 -1000

    Help commands

---
+++ MusicBot/Commands/HelpCommands.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Victoria;
+using Victoria.Enums;
+
+namespace MusicBot.Commands
+{
+    public class HelpCommands : ModuleBase<SocketCommandContext>
+    {
+        // [Command("help", RunMode = RunMode.Async)]
+        // [Summary("Lists all the commands")]
+        // public async Task HelpAsync([Remainder] string commandOrModule = null)
+        // {
+        //     await Context.Channel.SendMessageAsync("Help has arrived");
+        // }
+        
+    }
+}
