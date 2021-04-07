3367c2ec6545214644f6abec26b0f3256189b54c
Darth2499 <imsk8erboi@gmail.com>
2021-04-07 02:06:16 -1000

    Code cleanup I think...

---
+++ MusicBot/Commands/PremiumStatusCommands.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace MusicBot.Commands
+{
+    public class PremiumStatusCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("premiumstatus", RunMode = RunMode.Async)]
+        public async Task HelpAsync([Remainder] string commandOrModule = null)
+        {
+            var embed = new EmbedBuilder
+            {
+                Color = Discord.Color.DarkerGrey,
+                Title = "Premium status",
+                Description = "Your Subscription: `Free Forever`\n[Purchase](https://i.imgur.com/z4lM0Yj.gif) | [Manage](https://youtu.be/moZtoMP7HAA)",
+                ImageUrl = "https://i.imgur.com/aq7yRAn.gif"
+            }.Build();
+
+            await Context.Channel.SendMessageAsync(embed: embed);
+        }
+        
+    }
+}
