9d34ee69a77e0499e422bda0b6655a4eeadecdcd
Darth2499 <imsk8erboi@gmail.com>
2021-04-10 23:03:44 -1000

    Wrong number

--- MusicBot/Helpers/AudioHelper.cs
+++ MusicBot/Helpers/AudioHelper.cs
@@ -104,7 +104,7 @@ namespace MusicBot.Helpers
                         Url = $"{player.Track.Url}"
                     },
                     ImageUrl = thumb,
-                    Footer = new EmbedFooterBuilder { Text = $"{player.Queue.Count} song{(player.Queue.Count > 2 ? "s" : "")} in queue | Volume: {Program.Volume}%" }
+                    Footer = new EmbedFooterBuilder { Text = $"{player.Queue.Count} song{(player.Queue.Count > 1 ? "s" : "")} in queue | Volume: {Program.Volume}%" }
 
                 }.Build();
                 return embed;
