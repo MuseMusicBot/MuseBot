99fcee2eccb5a12cee7276475a3693f8217f2ea9
Darth2499 <imsk8erboi@gmail.com>
2021-04-16 01:01:14 -1000

    "I'm God, Okay" YouTube thumbnail switches HD & SD

--- MusicBot/Helpers/YouTubeHelper.cs
+++ MusicBot/Helpers/YouTubeHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using System.Net;
 
 namespace MusicBot.Helpers
 {
@@ -7,11 +8,21 @@ namespace MusicBot.Helpers
         public static string GetYtThumbnail(string url)
         {
             string baseUrl = "https://img.youtube.com/vi/";
-            string jpg = "/hqdefault.jpg";
+            string jpg = "/maxresdefault.jpg";
             Regex r = new Regex(@"https?:\/\/(?:www.)?(?:youtube|youtu)\.(?:com|be)\/?(?:watch\?v=)?(?<id>[A-z0-9_-]{1,11})", RegexOptions.Compiled);
             var match = r.Match(url);
-            string newUrl = baseUrl + match.Groups["id"].Value + jpg;
-            return newUrl;
+            string hdthumb = baseUrl + match.Groups["id"].Value + jpg;
+
+            try
+            {
+                byte[] imageData = new WebClient().DownloadData(hdthumb);
+                return hdthumb;
+            }
+            catch
+            {
+                string sdthumb = baseUrl + match.Groups["id"].Value + jpg.Replace("/maxresdefault.jpg", "/hqdefault.jpg");
+                return sdthumb;
+            }
         }
     }
 }
