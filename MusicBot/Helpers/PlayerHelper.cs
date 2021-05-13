10ba58f936ba5e6e83edd6256bfcacdb0efb7490
DrDevinRX <22141991+DrDevinRX@users.noreply.github.com>
2021-05-13 04:35:32 -0400

    Added PlayerHelper.cs

---
+++ MusicBot/Helpers/PlayerHelper.cs
@@ -0,0 +1,39 @@
+﻿using Victoria;
+using System.Threading.Tasks;
+
+namespace MusicBot.Helpers
+{
+    // Fill in the blanks :)
+    public class PlayerHelper
+    {
+        public async Task PauseResumeAsync(LavaPlayer player, bool pause = true)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task PreviousAsync(LavaPlayer player)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task NextTrackAsync(LavaPlayer player)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task LoopAsync(LavaPlayer player, bool loop = true)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task ShuffleAsync(LavaPlayer player)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task EjectAsync(LavaPlayer player)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
